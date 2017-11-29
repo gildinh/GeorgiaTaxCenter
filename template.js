9aae2b8e318813788057d77448a06b24de00a824
kennyhdinh <34103000+kennyhdinh@users.noreply.github.com>
2017-11-29 15:05:39 -0500

    Update template.js

--- template.js
+++ template.js
@@ -3,7 +3,10 @@
    as well as altering the navigation bar 
 */
 $(document).ready(function () {
-	if(window.location.href.indexOf("GXD") > -1 && window.location.hash && $('#l_n-1-2').length == 0){
+	if($("#caption_c-7").length > 0){
+        refreshKeep();
+		console.log("Page is refreshed inside a logon");
+	}else if(window.location.href.indexOf("GXD") > -1 && window.location.hash && $('#l_n-1-2').length == 0){
 		console.log("Going back from a request");
 		rndBgImage();
 		console.log("Background refreshed");
