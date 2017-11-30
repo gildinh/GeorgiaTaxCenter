d93697f8186ad05a51d17c36d1517b54be17c648
kennyhdinh <34103000+kennyhdinh@users.noreply.github.com>
2017-11-30 16:10:54 -0500

    Update template.js

--- template.js
+++ template.js
@@ -22,10 +22,11 @@ $(document).ready(function () {
         rndBgImage();    
 		console.log("Adds the Background for GXP");		
     }
-	
-	if($('button#d-8.DocFieldButton.FastEvtLinkClick').length > 0){
+	setTimeout(function(){}
+	if($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0){
 		console.log("This is found.");
-	}
+		}
+	}, 200);
 	
 	// Re-ADD BACKGROUND
 	if($('#l_n-1-2').length > 0 && window.location.hash){
