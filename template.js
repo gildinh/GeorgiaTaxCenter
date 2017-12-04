29c9dc05800ac2b3fbc11e5ab7ba3c9c531907e9
kennyhdinh <34103000+kennyhdinh@users.noreply.github.com>
2017-12-04 16:03:35 -0500

    Update template.js

--- template.js
+++ template.js
@@ -136,11 +136,11 @@ $(window).on("load", function(){
 });
 
 $(window).on('hashchange', function(){
-	if($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0 && window.location.hash !== '')
+	if($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0 && window.location.hash !== ''){
 		removeBGI();
 		console.log("Outside request will remove image.");
 		return false;
-	else{return true;}
+	}else{return true;}
 
 	if($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0){
 		rndBgImage();
