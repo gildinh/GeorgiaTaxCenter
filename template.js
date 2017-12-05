2bc10edcb296da22f640475229e48fc22303a46e
kennyhdinh <34103000+kennyhdinh@users.noreply.github.com>
2017-12-05 16:55:35 -0500

    Update template.js

--- template.js
+++ template.js
@@ -5,11 +5,11 @@ $(window).on("load", function(){
 		// Solution for View on Tap
 		if(window.location.hash == '' && $("ul.ManagerLogonOptions").length > 0){refreshKeep();return false;
 		// Loads Background Initially and upon refresh
-		}else if(($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0 && window.location.hash == '') || ($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0 && window.location.hash !== '') || ($('.SessionMessageContainer.BadWindowContainer').length > 0) || ($('.SessionMessageReturn.LoggedOffReturn').length > 0)){
+		}else if(($('#d-7.FieldEnabled.Field.DocControlPassword.FastEvtFieldKeyDown.FastFieldEnterEvent.FastEvtFieldFocus').length > 0 && window.location.hash == '') || ($('#d-8.DocFieldButton.FastEvtLinkClick').length > 0 && window.location.hash !== '') || ($('.SessionMessageContainer.BadWindowContainer').length > 0) || ($('.SessionMessageReturn.LoggedOffReturn').length > 0) || ){
 			rndBgImage();return false;
 			console.log("Getting Hit");
 		}else if($("ul.ManagerLogonOptions").length > 0 && ('onhashchange' in document) == false){refreshKeep();return false;
-		}else if($('#d-8.DocFieldButton.FastEvtLinkClick').length == 0 && window.location.hash == ''){removeBG();return false;}
+		}else if($('#d-7.FieldEnabled.Field.DocControlPassword.FastEvtFieldKeyDown.FastFieldEnterEvent.FastEvtFieldFocus').length == 0 && window.location.hash == ''){removeBG();return false;}
 	},200);
 	removeBGImage();
 });
